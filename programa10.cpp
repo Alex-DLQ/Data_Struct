2228b39fd787f84bf7aa18b51074b104c95d0d8b
Alex <alexdannisl@gmail.com>
2025-05-08 12:57:12 -0500

    Modificamos el programa 10

--- programa10.cpp
+++ programa10.cpp
@@ -3,157 +3,186 @@
 #include <cstdlib>
 using namespace std;
 
-struct lapicero{
+struct lapicero {
     char tipo_punta[20];
     char color[20];
     char marca[20];
-    int precio;
-}lapicero1[3];
+    float precio;
+} lapicero1[3];
 
-struct carpeta{
+struct carpeta {
     char color[20];
-    char formar[20];
-    int precio;
+    char forma[20]; // corregido: antes estaba mal escrito como "formar"
+    float precio;
     char marca[20];
-}carpeta1[3];
+} carpeta1[3];
 
-struct mochila{
+struct mochila {
     char tamanio[20];
     char forma[20];
     char marca[20];
-    int precio;
-}mochila1[3];
+    float precio;
+} mochila1[3];
 
-struct lenguaje{
+struct lenguaje {
     char nombre[20];
     char tipado[20];
-}lenguaje1[3];
+} lenguaje1[3];
 
-struct laptop{
+struct laptop {
     char almacenamiento[20];
     char procesador[20];
     char memoria[20];
-    int precio;
+    float precio;
     char marca[20];
-}laptop1[3];
+} laptop1[3];
 
-struct servidorWeb{
+struct servidorWeb {
     char escalabilidad[20];
     char disponibilidad[20];
-}servidorWeb1[3];
+} servidorWeb1[3];
 
-struct usb{
+struct usb {
     char marca[20];
     char version[20];
     char almacenamiento[20];
-}usb1[3];
+} usb1[3];
 
-struct cableUTP{
+struct cableUTP {
     char categoria[20];
     char velocidad[20];
     char marca[20];
-    int precio;
-}cableUTP1[3];
+    float precio;
+} cableUTP1[3];
 
-int main(){
+int main() {
     int opcion;
-    cout << "Dijite un valor para realizar la secuencia de relleno de structuras"<<endl;
-    cout<< "(1 = lapicero) (2 = carpeta) (3 = mochila) (4 = lenguaje)"<<endl; 
-    cout<<"(5 = laptop) (6 = servidor web) (7 = usb) (8 = cable utp)"<<endl;
+    cout << "Digite un valor para realizar la secuencia de relleno de estructuras" << endl;
+    cout << "(1 = lapicero) (2 = carpeta) (3 = mochila) (4 = lenguaje)" << endl;
+    cout << "(5 = laptop) (6 = servidor web) (7 = usb) (8 = cable utp)" << endl;
     cin >> opcion;
-    switch(opcion){
+    cin.ignore(); // Limpia el buffer después de leer int
+
+    switch (opcion) {
         case 1:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese el tipo de punta "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese el tipo de punta " << i + 1 << ": ";
                 cin >> lapicero1[i].tipo_punta;
-                cout << "Ingrese el color "<<i + 1<<": ";
+                cout << "Ingrese el color " << i + 1 << ": ";
                 cin >> lapicero1[i].color;
-                cout << "Ingrese la marca "<<i + 1<<": ";
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> lapicero1[i].marca;
-                cout << "Ingrese el precio "<<i + 1<<": ";
+                cout << "Ingrese el precio " << i + 1 << ": ";
                 cin >> lapicero1[i].precio;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 2:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese el color "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese el color " << i + 1 << ": ";
                 cin >> carpeta1[i].color;
-                cout << "Ingrese la forma "<<i + 1<<": ";
-                cin >> carpeta1[i].formar;
-                cout << "Ingrese el precio "<<i + 1<<": ";
+                cout << "Ingrese la forma " << i + 1 << ": ";
+                cin >> carpeta1[i].forma;
+                cout << "Ingrese el precio " << i + 1 << ": ";
                 cin >> carpeta1[i].precio;
-                cout << "Ingrese la marca "<<i + 1<<": ";
+                cin.ignore();
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> carpeta1[i].marca;
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 3:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese el tamanio "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese el tamanio " << i + 1 << ": ";
                 cin >> mochila1[i].tamanio;
-                cout << "Ingrese la forma "<<i + 1<<": ";
+                cout << "Ingrese la forma " << i + 1 << ": ";
                 cin >> mochila1[i].forma;
-                cout << "Ingrese la marca "<<i + 1<<": ";
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> mochila1[i].marca;
-                cout << "Ingrese el precio "<<i + 1<<": ";
+                cout << "Ingrese el precio " << i + 1 << ": ";
                 cin >> mochila1[i].precio;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 4:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese el nombre "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese el nombre " << i + 1 << ": ";
                 cin >> lenguaje1[i].nombre;
-                cout << "Ingrese el tipado "<<i + 1<<": ";
+                cout << "Ingrese el tipado " << i + 1 << ": ";
                 cin >> lenguaje1[i].tipado;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 5:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese el almacenamiento "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese el almacenamiento " << i + 1 << ": ";
                 cin >> laptop1[i].almacenamiento;
-                cout << "Ingrese el procesador "<<i + 1<<": ";
+                cout << "Ingrese el procesador " << i + 1 << ": ";
                 cin >> laptop1[i].procesador;
-                cout << "Ingrese la memoria "<<i + 1<<": ";
+                cout << "Ingrese la memoria " << i + 1 << ": ";
                 cin >> laptop1[i].memoria;
-                cout << "Ingrese el precio "<<i + 1<<": ";
+                cout << "Ingrese el precio " << i + 1 << ": ";
                 cin >> laptop1[i].precio;
-                cout << "Ingrese la marca "<<i + 1<<": ";
+                cin.ignore();
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> laptop1[i].marca;
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 6:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese la escalabilidad "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese la escalabilidad " << i + 1 << ": ";
                 cin >> servidorWeb1[i].escalabilidad;
-                cout << "Ingrese la disponibilidad "<<i + 1<<": ";
+                cout << "Ingrese la disponibilidad " << i + 1 << ": ";
                 cin >> servidorWeb1[i].disponibilidad;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 7:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese la marca "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> usb1[i].marca;
-                cout << "Ingrese la version "<<i + 1<<": ";
+                cout << "Ingrese la version " << i + 1 << ": ";
                 cin >> usb1[i].version;
-                cout << "Ingrese el almacenamiento "<<i + 1<<": ";
+                cout << "Ingrese el almacenamiento " << i + 1 << ": ";
                 cin >> usb1[i].almacenamiento;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con éxito.\n";
             break;
         case 8:
-            for(int i = 0; i < 3; i++){
-                cout << "Ingrese la categoria "<<i + 1<<": ";
+            for (int i = 0; i < 3; i++) {
+                cout << "Ingrese la categoria " << i + 1 << ": ";
                 cin >> cableUTP1[i].categoria;
-                cout << "Ingrese la velocidad "<<i + 1<<": ";
+                cout << "Ingrese la velocidad " << i + 1 << ": ";
                 cin >> cableUTP1[i].velocidad;
-                cout << "Ingrese la marca "<<i + 1<<": ";
+                cout << "Ingrese la marca " << i + 1 << ": ";
                 cin >> cableUTP1[i].marca;
-                cout << "Ingrese el precio "<<i + 1<<": ";
+                cout << "Ingrese el precio " << i + 1 << ": ";
                 cin >> cableUTP1[i].precio;
+                cin.ignore();
+                cout << endl;
             }
+            cout << "\nDatos registrados con exito.\n";
             break;
         default:
             cout << "Opcion no valida" << endl;
             break;
     }
+
     system("pause");
+    cout << endl;
     return 0;
 }
+
